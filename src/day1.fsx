cf260dd36e193d76e0541b9edc7acfaf857dcdcb
Nestor Fabian Villar <nesfvillar@gmail.com>
2023-08-31 22:51:05 -0300

    Finished day 1 part 2

--- src/day1.fsx
+++ src/day1.fsx
@@ -1,3 +1,5 @@
+module Day1
+
 let input =
     "inputs/day1.txt"
     |> System.IO.File.ReadAllLines
@@ -5,4 +7,13 @@ let input =
 
 let Part1 (input: seq<int>) = Seq.sum input
 
-printfn "Part 1 solution: %d" (Part1 input)
+let repeatSeq items = seq { while true do yield! items }
+
+let Part2 (input: seq<int>) =
+    let input = repeatSeq input
+    let items = Seq.scan (fun acc elem -> acc + elem) 0 input
+    let seen = items |> Seq.scan (fun (acc: int Set) elem -> acc.Add elem) Set.empty
+
+    Seq.zip items seen
+    |> Seq.find (fun (value, seen) -> seen.Contains value)
+    |> fst
