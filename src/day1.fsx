81db5427c41ecfc16f7940ee33b4a3e17ccad6c4
Nestor Fabian Villar <nesfvillar@gmail.com>
2023-09-06 16:15:43 -0300

    Applied formatter

--- src/day1.fsx
+++ src/day1.fsx
@@ -7,12 +7,14 @@ let input =
 
 let Part1 (input: int seq) = Seq.sum input
 
-let repeatSeq items = seq { while true do yield! items }
+let repeatSeq items =
+    seq {
+        while true do
+            yield! items
+    }
 
 let Part2 (input: int seq) =
     let items = repeatSeq input |> Seq.scan (fun acc elem -> acc + elem) 0
     let seen = items |> Seq.scan (fun (acc: int Set) elem -> acc.Add elem) Set.empty
 
-    Seq.zip items seen
-    |> Seq.find (fun (elem, seen) -> seen.Contains elem)
-    |> fst
+    Seq.zip items seen |> Seq.find (fun (elem, seen) -> seen.Contains elem) |> fst
